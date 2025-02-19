181705c010c680cf16880a2076b41ce1e9b1049c
2003Rajansahni <rajansahni2003@gmail.com>
2025-02-19 09:45:03 +0530

    Production ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,53 @@
-// client/src/App.js
+// // client/src/App.js
+
+// import './App.css';
+// import { useState } from 'react';
+// import Home from './pages/home';
+// import Chat from './pages/chat';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import io from 'socket.io-client';
+// //import Sender from './pages/harish/Sender';
+// //import Recieveer from './pages/harish/Reciever';
+// //import Navbar from './pages/harish/Navbar';
+
+// const socket = io.connect('http://localhost:4000');
+
+// function App() {
+//   const [username, setUsername] = useState('');
+//   const [room, setRoom] = useState('');
+
+//   return (
+//     <Router>
+//       <div className='App'>
+  
+//         <Routes>
+//           <Route
+//             path='/'
+//             element={
+//               <Home
+//                 username={username}
+//                 setUsername={setUsername}
+//                 room={room}
+//                 setRoom={setRoom}
+//                 socket={socket}
+//               />
+//             }
+//           />
+//           {/* Add this */}
+//           <Route
+//             path='/chat'
+//             element={<Chat username={username} room={room} socket={socket} />}
+//           />
+          
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 
 import './App.css';
 import { useState } from 'react';
@@ -6,11 +55,8 @@ import Home from './pages/home';
 import Chat from './pages/chat';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import io from 'socket.io-client';
-//import Sender from './pages/harish/Sender';
-//import Recieveer from './pages/harish/Reciever';
-//import Navbar from './pages/harish/Navbar';
 
-const socket = io.connect('http://localhost:4000');
+const socket = io.connect(process.env.REACT_APP_SOCKET_SERVER);
 
 function App() {
   const [username, setUsername] = useState('');
@@ -19,7 +65,6 @@ function App() {
   return (
     <Router>
       <div className='App'>
-  
         <Routes>
           <Route
             path='/'
@@ -33,12 +78,10 @@ function App() {
               />
             }
           />
-          {/* Add this */}
           <Route
             path='/chat'
             element={<Chat username={username} room={room} socket={socket} />}
           />
-          
         </Routes>
       </div>
     </Router>
@@ -46,3 +89,4 @@ function App() {
 }
 
 export default App;
+
